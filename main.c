3850a05a4e886c5e9437555b8342486f99c027a2
Asif <119930322+a1rzayev@users.noreply.github.com>
2024-04-21 16:29:20 +0400

    Add changing turn in new game

--- main.c
+++ main.c
@@ -5,6 +5,8 @@ int main() {
 
     int currentPlayer; // 0 for human, 1 for computer
     int column;
+    int timePlaying = 1;
+    int startingPlayer;
     int colorChoise; //choise of turn at the start of the game
     do
     {   
@@ -15,10 +17,17 @@ int main() {
             scanf("%d", &colorChoise);
         } while (colorChoise != 1 && colorChoise != 0);
 
-        do { // safety defining of starting choise, to not get any wrong information
-            printf("Choose the first player([0] - You, [1] - Computer): ");
-            scanf("%d", &currentPlayer);
-        } while (currentPlayer != 1 && currentPlayer != 0);
+        if(timePlaying == 1){
+            do { // safety defining of starting choise, to not get any wrong information
+                printf("Choose the first player([0] - You, [1] - Computer): ");
+                scanf("%d", &currentPlayer);
+            } while (currentPlayer != 1 && currentPlayer != 0);
+            startingPlayer = currentPlayer;
+        }
+        else {
+            setTurn(&startingPlayer);
+            currentPlayer = startingPlayer;
+        }
 
         do  {
             displayGrid();
@@ -34,7 +43,7 @@ int main() {
             if (isMovePossible(column)) {
 
                 dropDisc(column, ((currentPlayer) ?  slots[1-colorChoise] : slots[colorChoise]));
-                currentPlayer = 1 - currentPlayer; // Switch player
+                setTurn(&currentPlayer);
             } else {
                 printf("Invalid move! Try again.\n");
             }
@@ -49,6 +58,7 @@ int main() {
             printf("Do you want to play again?([0] - No, [1] - Yes): ");
             scanf("%d", &wantToRestart);
         } while (wantToRestart != 1 && wantToRestart != 0);
+        ++timePlaying;
     } while (wantToRestart);
 
     return 0;
